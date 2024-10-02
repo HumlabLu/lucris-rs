151744c90a9f0beac6d8dfac44d3028796a55c4e
Peter Berck <peter.berck@humlab.lu.se>
2024-10-02 12:25:51 +0200

    Initial.

---
+++ src/json.rs
@@ -0,0 +1,124 @@
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+
+// Catch-all type for undefined fields in the structures.
+// These are caught by "#[serde(flatten)]".
+type Other = serde_json::Map<String, serde_json::Value>;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Title {
+    formatted: Option<bool>,
+    value: String,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct NameText {
+    locale: Option<String>,
+    value: String,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Name {
+    formatted: bool,
+    text: Vec<NameText>,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct OrganisationalUnit {
+    uuid: String,
+    externally_managed: bool,
+    name: Name,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Journal {
+    uuid: String,
+    name: Name,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct JournalAssociation {
+    pure_id: u64,
+    title: Title,
+    journal: Journal,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Info {
+    created_date: String,
+    modified_date: String,
+    portal_url: String,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PublicationStatus {
+    pure_id: u64,
+    current: bool,
+    #[serde(flatten)]
+    other: Other,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct OneJson {
+    pure_id: u64,
+    uuid: String,
+    title: Title,
+    peer_review: bool,
+    managing_organisational_unit: OrganisationalUnit,
+    confidential: bool,
+    info: Info,
+    total_scopus_citations: u32,
+    pages: String,
+    volume: String,
+    journal_association: JournalAssociation,
+    journal_number: String,
+    publication_statuses: Vec<PublicationStatus>,
+    #[serde(flatten)]
+    other: Other,
+}
+
+// Function to read the JSON file
+pub fn read_json(file_path: &str) -> Result<OneJson, Box<dyn std::error::Error>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    /*
+    let fd = std::fs::File::open("logs").unwrap();
+    let x = std::io::BufReader::new(fd);
+
+    x
+        .lines()        // split to lines serially
+        .filter_map(|line: Result<String, _>| line.ok())
+        .par_bridge()   // parallelize
+        .filter_map(|line: String| serde_json::from_str(&line).ok()) // filter out bad lines
+        .for_each(|v: LogLine| {
+           // do some processing (in parallel)
+           println!("X={}", v.f1);
+        });
+    */
+    let data: OneJson = serde_json::from_reader(reader)?;
+    Ok(data)
+}
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use clap::{Parser};
+mod json;
+use json::read_json;
+
+#[macro_use]
+extern crate simple_log;
+use simple_log::LogConfigBuilder;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Research info jasonl file
+    #[arg(short, long, help="The file containing the cleaned research-outputs.")]
+    research: Option<String>,
+    
+    /// Sets the level of logging;
+    /// error (highest priority), warn, info, debug, or trace
+    #[arg(short, long, default_value = "warn")]
+    log_level: String,
+}
+
+fn main() -> Result<(), String> {
+    let cli = Cli::parse();
+
+    let config = LogConfigBuilder::builder()
+        .path("./lucris.log")
+        .output_file()
+        .level(cli.log_level)?
+        .time_format("%Y-%m-%d %H:%M:%S")
+        .output_console()
+        .build();
+    simple_log::new(config)?;
+    debug!("Starting lucris-rs.");
+
+    if let Some(research_filename) = cli.research {
+        info!("Research file {:?}.", research_filename);
+        match read_json("one.json") {
+            Ok(data) => println!("{:?}", data),
+            Err(e) => eprintln!("Error reading JSON: {}", e),
+        }
+    }
+    
+    debug!("Ending lucris-rs.");
+    Ok(())
+}
