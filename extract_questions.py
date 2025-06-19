f41765990daa0b0101c87a9a1815b7b8357e436c
Peter Berck <peter.berck@humlab.lu.se>
2025-06-19 16:59:51 +0200

    Script to parse logfile added.

---
+++ extract_questions.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import argparse
+
+# Get the questions and answers from the Pufendorf chatbot.
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", help="Filename of log file.", default="lucrisbot.log")
+args = parser.parse_args()
+
+in_q = False
+in_a = False
+with open(args.filename, "r") as f:
+    for line in f:
+        line = line.strip()
+        bits = line.split()
+        if len(bits) > 5:
+            bits = bits[5:]
+            if bits[0] == "USER:":
+                #print("-" * 40)
+                in_q = True
+                print("")
+                print("QUESTION")
+                print(" ".join(bits[1:]))
+                continue
+            if in_q:
+                if bits[0] == "FULL":
+                    in_q = False
+                if bits[0] == "TEMP:":
+                    in_a = True
+                    in_q = False
+                    print("ANSWER")
+                    continue
+            if not in_q:
+                if bits[0] == "IGNORE":
+                    in_q = True
+            if in_a or in_q:
+                if bits[0].startswith("2025"): # Remove date
+                    in_q = False
+                    in_a = False
+            if in_q:
+                #print("\t", " ".join(bits))
+                pass
+            if in_a:
+                print(" ".join(bits))
+            
+            
