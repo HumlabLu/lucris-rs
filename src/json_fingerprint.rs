e57979fa3b8585ccf983fdcec13a7167c8c78117
Peter Berck <peter.berck@humlab.lu.se>
2025-08-28 14:30:30 +0200

    More tests.

--- src/json_fingerprint.rs
+++ src/json_fingerprint.rs
@@ -20,7 +20,7 @@ pub struct FingerprintJson {
     pub uuid: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Concept {
     pub frequency: Option<f64>,
     pub rank: Option<f64>,
@@ -40,7 +40,7 @@ pub struct Info {
 
 impl FingerprintJson {
     /// Return (content id, list of concept uuids) where id = contentUuid or fallback uuid
-    pub fn id_and_concept_uuids(&self) -> Option<(String, Vec<String>)> {
+    pub fn _id_and_concept_uuids(&self) -> Option<(String, Vec<String>)> {
         let id = self.contentUuid.as_ref().or(self.uuid.as_ref())?.clone();
 
         let concept_ids = self
@@ -51,6 +51,35 @@ impl FingerprintJson {
             .collect::<Vec<_>>();
 
         Some((id, concept_ids))
+    }
+
+    pub fn id_and_concept_uuids(&self) -> Option<(String, Vec<(String, f64)>)> {
+        let id = self.contentUuid.as_ref().or(self.uuid.as_ref())?.clone();
+
+        let concepts = self.concepts.as_ref()?;
+        let concept_pairs = concepts
+            .iter()
+            .filter_map(|c| {
+                let uuid = c.uuid.as_ref()?;
+                let rank = c.weightedRank.unwrap_or(0.0) as f64;
+                Some((uuid.clone(), rank))
+            })
+            .collect::<Vec<_>>();
+
+        Some((id, concept_pairs))
+    }
+
+    pub fn id_and_concepts(&self) -> Option<(String, Vec<Concept>)> {
+        let id = self.contentUuid.as_ref().or(self.uuid.as_ref())?.clone();
+
+        let concepts = self.concepts.as_ref()?.clone();
+        Some((id, concepts))
+    }
+
+    pub fn id_and_concepts_ref(&self) -> Option<(&str, &[Concept])> {
+        let id = self.contentUuid.as_deref().or(self.uuid.as_deref())?;
+        let concepts = self.concepts.as_deref()?;
+        Some((id, concepts))
     }
 }
 
