3925278b252197569c699e67584c2ddacd8c05cc
Peter Berck <peter.berck@humlab.lu.se>
2026-08-05 18:04:31 +0200

    Added fixbom.py to fix utf-8 problems.

---
+++ fixbom.py
@@ -0,0 +1,10 @@
+import codecs
+from pathlib import Path
+
+source = Path("research_20260805.m.csv")
+data = source.read_bytes().removeprefix(codecs.BOM_UTF8)
+source.write_bytes(codecs.BOM_UTF8 + data)
+
+source = Path("research_20260805.u.csv")
+data = source.read_bytes().removeprefix(codecs.BOM_UTF8)
+source.write_bytes(codecs.BOM_UTF8 + data)
