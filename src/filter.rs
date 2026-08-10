fa236770d24f57e17e12caf8a22c19fb836afc82
Peter Berck <peter.berck@humlab.lu.se>
2026-08-10 17:41:06 +0200

    Made the filer sace-insensitive.

--- src/filter.rs
+++ src/filter.rs
@@ -7,19 +7,28 @@ pub enum PersonFilterMode {
     DeleteMatching,
 }
 
+impl ResearchClean {
+    pub fn has_person(&self, names: &HashSet<String>) -> bool {
+        self.persons
+            .iter()
+            .any(|person| names.contains(&person.get_name().trim().to_lowercase()))
+    }
+}
+
 pub fn filter_research_by_person(
     research: &mut HashMap<String, ResearchClean>,
     names: Vec<String>,
     mode: PersonFilterMode,
 ) {
     // Convert so we can use contain().
-    let names: HashSet<String> = names.into_iter().collect();
+    // let names: HashSet<String> = names.into_iter().collect();
+    let names: HashSet<String> = names
+        .into_iter()
+        .map(|name| name.trim().to_lowercase())
+        .collect();
 
     research.retain(|_, item| {
-        let matches = item
-            .persons
-            .iter()
-            .any(|person| names.contains(person.get_name()));
+        let matches = item.has_person(&names);
 
         match mode {
             PersonFilterMode::KeepMatching => matches,
